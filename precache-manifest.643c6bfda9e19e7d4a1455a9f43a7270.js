bbf460b269a94b291aa621772a3b50e08cf1ca5a
Rémi Testa <remi.testa@gmail.com>
2019-06-04 19:31:53 +0200

    Updates

---
+++ precache-manifest.643c6bfda9e19e7d4a1455a9f43a7270.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd7ef701f3e8002db2dcf3db7fae1ba",
+    "url": "/parking/index.html"
+  },
+  {
+    "revision": "4aaf27025c387bfed629",
+    "url": "/parking/static/css/main.e8dc1d25.chunk.css"
+  },
+  {
+    "revision": "08812d3d25ee22106034",
+    "url": "/parking/static/js/2.683f65be.chunk.js"
+  },
+  {
+    "revision": "4aaf27025c387bfed629",
+    "url": "/parking/static/js/main.2fb6ecdb.chunk.js"
+  },
+  {
+    "revision": "e81344eab7a705a666cc",
+    "url": "/parking/static/js/runtime~main.9c5cc316.js"
+  }
+]);
