7f7d0bbcdf6c4450d45f49ca33a8cf1144164f0f
Rémi Testa <remi.testa@gmail.com>
2019-06-04 19:48:30 +0200

    fix image

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="container">
-          <img src="%PUBLIC_URL%/montpellier-logo.png" alt="Montpellier Parkings" />
+          <img src="montpellier-logo.png" alt="Montpellier Parkings" />
           <br/>
           <br/>
           <h1 className="refresh">Parkings Montpellier Métropole</h1>
